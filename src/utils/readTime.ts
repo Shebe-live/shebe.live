8afeb9041c7ec02a1a12a86b4ee945bba425ea58
Alexey Peterson <contact@peterson.by>
2024-07-04 21:53:08 +0300

    Fix: localization & layout

--- src/utils/readTime.ts
+++ src/utils/readTime.ts
@@ -7,9 +7,18 @@ import { toString } from 'mdast-util-to-string'
 export function remarkReadingTime() {
 	return function (tree: unknown, { data }: any) {
 		const textOnPage = toString(tree)
-		const readingTime = getReadingTime(textOnPage)
-		// readingTime.text will give us minutes read as a friendly string,
-		// i.e. "3 min read"
-		data.astro.frontmatter.minutesRead = readingTime.text
+		const { minutes } = getReadingTime(textOnPage)
+
+		function minutesWord(minutes: Number) {
+			if (minutes === 1) {
+				return `Время чтения: ${minutes} минута`;
+			} else if (Number(minutes) < 5) {
+				return `Время чтения: ${minutes} минуты`;
+			}
+
+			return `Время чтения: ${minutes} минут`
+		}
+
+		data.astro.frontmatter.minutesRead = minutesWord(minutes)
 	}
 }
